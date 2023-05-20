daa5e1ad889892f155b0345e5c88ba5376c49536
Caetano D <127361836+Kkanzaki@users.noreply.github.com>
2023-05-20 14:39:29 -0400

    Update exercicio

--- pythontest/ex-lanche.py
+++ pythontest/ex-lanche.py
@@ -1,8 +1,8 @@
 pedido=input('Digite a opçao desejada \n1 para Big Super Sanduba \n2 para Quase Super Sanduba \n3 para Mirradus Sanduba\n')
 
-big=['opção1','R$5,00']
-qs=['opção2','R$3,00']
-ms=['opção3','R$1,50']
+big=['opção 1','R$5,00']
+qs=['opção 2','R$3,00']
+ms=['opção 3','R$1,50']
 
 if pedido == '1':
     print('Você pediu a',big[0],'que custa R$',big[1])
